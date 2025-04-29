636e4c10dd95b7ef0c830b9766eab79a231c8a6b
aradcharon <aradshafiee06@gmail.com>
2025-04-29 11:59:46 +0330

    add constructor for Accessory class

--- src/products/Accessory.java
+++ src/products/Accessory.java
@@ -5,4 +5,11 @@ public class Accessory extends Product{
 
     private static int accessoryCounter = 1;
 
+    public Accessory(String title, Double price, String color) {
+        super(title, price);
+        this.color = color;
+        accessoryCounter++;
+    }
+
+    
 }
