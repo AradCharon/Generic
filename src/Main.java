755c7696fd0a48bceaf4fbd6223a43f54136e9c5
aradcharon <aradshafiee06@gmail.com>
2025-04-29 17:05:23 +0330

    add all import and calculateTotalPrice method to main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import products.Product;
+import inventory.Inventory;
+import products.Accessory;
+import products.Book;
+import products.Notebook;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        public static double calculateTotalPrice (Inventory<? extends Product> inventory){
+            double total = 0;
+            for (Product item : inventory.getItems()){
+                total += item.getPrice;
+            }
+
+            return total;
+        }
     }
 }
