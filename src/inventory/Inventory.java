2ee0ecdf07e7e2edb199895323a18e77e74f0e68
aradcharon <aradshafiee06@gmail.com>
2025-04-29 12:18:46 +0330

    add constructor and getters and setters for Inventory class

--- src/inventory/Inventory.java
+++ src/inventory/Inventory.java
@@ -5,4 +5,16 @@ import java.util.ArrayList;
 
 public class Inventory<T extends Product> {
     private ArrayList<T> items;
+
+    public Inventory() {
+        this.items = new ArrayList<>();
+    }
+
+    public ArrayList<T> getItem{
+        return items;
+    }
+
+    public void setItems(ArrayList<T> items){
+        this.items = items;
+    }
 }
