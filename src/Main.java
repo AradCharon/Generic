d432282a86d2d934c2537d8eb00b5ba360eaf609
aradcharon <aradshafiee06@gmail.com>
2025-04-29 17:08:43 +0330

    add three inventory to main

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,10 @@ import products.Notebook;
 
 public class Main {
     public static void main(String[] args) {
+        Inventory<Book> bookInventory = new Inventory<>();
+        Inventory<Notebook> notebookInventoryInventory = new Inventory<>();
+        Inventory<Accessory> accessoryInventory = new Inventory<>();
+
         public static double calculateTotalPrice (Inventory<? extends Product> inventory){
             double total = 0;
             for (Product item : inventory.getItems()){
