e76a107b586893f7359f6a80d4ba9effb5da2cfb
aradcharon <aradshafiee06@gmail.com>
2025-04-29 11:23:12 +0330

    add toString methods and constructor for Notebook class

--- src/products/Notebook.java
+++ src/products/Notebook.java
@@ -6,5 +6,22 @@ public class Notebook extends Product{
 
     private static int notebookCounter = 1;
 
+    public Notebook(String title, Double price, int pageCount, boolean isHardCover) {
+        super(title, price);
+        this.pageCount = pageCount;
+        this.isHardCover = isHardCover;
+        notebookCounter++;
+    }
     
+    @Override
+    public String toString(){
+        String coverType;
+        if (isHardCover) {
+            coverType = "Yes";
+        }
+        else {
+            coverType = "No";
+        }
+        return super.toString() + ", Page Count: " + pageCount + ", Hard Cover: " + coverType;
+    }
 }
