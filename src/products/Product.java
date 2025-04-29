039538b7b4e97fc8e9bbcdf033744be4206f9857
aradcharon <aradshafiee06@gmail.com>
2025-04-29 10:24:12 +0330

    add all fields amd getters and setters methods for Product class

--- src/products/Product.java
+++ src/products/Product.java
@@ -1,4 +1,33 @@
 package products;
 
-public class Product {
+public abstract class Product {
+    private String title;
+    private Double price;
+    private String id;
+
+    public Product(String title, double price) {
+        this.title = title;
+        this.price = price;
+        this.id = generateId();
+    }
+
+    public String getTitle{
+        return title;
+    }
+
+    public String setTitle(String title){
+        this.title = title;
+    }
+
+    public String getPrice{
+        return price;
+    }
+
+    public String setPrice(String title){
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
 }
